19c07222c17f3ef72562c18addcc2656488dec78
palewire <ben.welsh@gmail.com>
2014-01-23 13:31:28 -0800

    Created a new templatetag that wraps the first character in a string in a .dropcap CSS class.

--- toolbox/tests.py
+++ toolbox/tests.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from django.test import TestCase
+from .templatetags.toolbox_tags import dropcap
 
 
 class ToolboxTest(TestCase):
-    
-    TEST_CSV = """Name,Type,County
-La Cañada Flintridge,Neighborhood,L.A.County
-Downtown,Neighborhood,L.A.County
-"""
 
     def test_unicodecsv(self):
         """
@@ -17,8 +14,23 @@ Downtown,Neighborhood,L.A.County
         import csv
         from toolbox import unicodecsv
         from cStringIO import StringIO
-        d = StringIO(self.TEST_CSV)
+        d = StringIO("""Name,Type,County
+La Cañada Flintridge,Neighborhood,L.A.County
+Downtown,Neighborhood,L.A.County
+""")
         reader = unicodecsv.UnicodeDictReader(d)
         reader.next()
         reader.__iter__()
         self.assertEqual(type(list(reader)), type([]))
+
+    def test_dropcap_filter(self):
+        """
+        Test simple usage of the capfirst templatetag.
+        """
+        before = "I love dropcaps."
+        after = dropcap(before)
+        print after
+        self.failUnlessEqual(
+            after,
+            "<span class='dropcap'>I</span> love dropcaps."
+        )
